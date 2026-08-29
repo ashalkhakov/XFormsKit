9d63c27fd4a90594de521ef2ec7f88bdb843dd11
Artyom Shalkhakov <artyom.shalkhakov@gmail.com>
2026-08-29 23:03:25 +0500

    Implement XPath 1.0 lexer, parser, and evaluator for the hello slice.

--- Sources/XFormsKit/XPath/XFXPath.m
+++ Sources/XFormsKit/XPath/XFXPath.m
@@ -8,3 +8,41 @@
 #import <Foundation/NSXMLNode.h>
 #import <Foundation/NSXMLElement.h>
 #import <Foundation/NSXMLDocument.h>
+
+typedef NS_ENUM(NSInteger, XFXPathTokenKind) {
+    XFXPathTokenEOF = 0,
+    XFXPathTokenName,
+    XFXPathTokenString,
+    XFXPathTokenNumber,
+    XFXPathTokenSlash,
+    XFXPathTokenSlashSlash,
+    XFXPathTokenAt,
+    XFXPathTokenDot,
+    XFXPathTokenDotDot,
+    XFXPathTokenStar,
+    XFXPathTokenLParen,
+    XFXPathTokenRParen,
+    XFXPathTokenLBrack,
+    XFXPathTokenRBrack,
+    XFXPathTokenComma,
+    XFXPathTokenPlus,
+    XFXPathTokenMinus,
+    XFXPathTokenEq,
+    XFXPathTokenNe,
+    XFXPathTokenLt,
+    XFXPathTokenGt,
+    XFXPathTokenLe,
+    XFXPathTokenGe,
+    XFXPathTokenAnd,
+    XFXPathTokenOr,
+    XFXPathTokenUnion
+};
+
+@interface XFXPathToken : NSObject
+@property (nonatomic, assign) XFXPathTokenKind kind;
+@property (nonatomic, copy) NSString *text;
+@property (nonatomic, assign) double number;
+@end
+
+@implementation XFXPathToken
+@end
