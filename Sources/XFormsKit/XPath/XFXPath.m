4b31ab32ed6e19dd2ae813ce3fc6e60c3238900a
Artyom Shalkhakov <artyom.shalkhakov@gmail.com>
2026-08-29 23:03:53 +0500

    Complete XPath 1.0 evaluator implementation.

--- Sources/XFormsKit/XPath/XFXPath.m
+++ Sources/XFormsKit/XPath/XFXPath.m
@@ -46,3 +46,60 @@ typedef NS_ENUM(NSInteger, XFXPathTokenKind) {
 
 @implementation XFXPathToken
 @end
+
+#pragma mark - AST
+
+@interface XFExpr : NSObject
+- (XFXPathValue *)eval:(XFExprContext *)ctx error:(NSError **)error;
+@end
+
+@implementation XFExpr
+- (XFXPathValue *)eval:(XFExprContext *)ctx error:(NSError **)error
+{
+    (void)ctx;
+    if (error) {
+        *error = [NSError errorWithDomain:XFErrorDomain
+                                     code:XFErrorXPathEvaluation
+                                 userInfo:@{ NSLocalizedDescriptionKey: @"abstract expression" }];
+    }
+    return nil;
+}
+@end
+
+@interface XFLiteralExpr : XFExpr
+@property (nonatomic, strong) XFXPathValue *value;
+@end
+
+@implementation XFLiteralExpr
+- (XFXPathValue *)eval:(XFExprContext *)ctx error:(NSError **)error
+{
+    (void)ctx; (void)error;
+    return self.value;
+}
+@end
+
+@interface XFFunctionExpr : XFExpr
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSArray<XFExpr *> *args;
+@end
+
+@interface XFPathExpr : XFExpr
+@property (nonatomic, assign) BOOL absolute;
+@property (nonatomic, assign) BOOL descendantOrSelfFirst;
+@property (nonatomic, copy) NSArray *steps;
+@end
+
+@interface XFStep : NSObject
+@property (nonatomic, copy) NSString *axis;
+@property (nonatomic, copy) NSString *test;
+@property (nonatomic, copy) NSArray<XFExpr *> *predicates;
+@end
+
+@implementation XFStep
+@end
+
+@interface XFBinaryExpr : XFExpr
+@property (nonatomic, copy) NSString *op;
+@property (nonatomic, strong) XFExpr *left;
+@property (nonatomic, strong) XFExpr *right;
+@end
